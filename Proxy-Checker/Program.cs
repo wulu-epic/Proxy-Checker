5a26506e2ab45deb2c773a505322e9529096d240
wulu-epic <70172469+wulu-epic@users.noreply.github.com>
2022-02-05 13:10:43 +0000

    added threads added error catching

--- Proxy-Checker/Program.cs
+++ Proxy-Checker/Program.cs
@@ -15,11 +15,18 @@ namespace Proxy_Checker
     {
         public static SynchronizedCollection<string> untested = new SynchronizedCollection<string>();
         public static SynchronizedCollection<string> working = new SynchronizedCollection<string>();
+        public static List<string> wo = new List<string>();
+        public static RichTextBox rtb = new RichTextBox();
 
         public static int remaining;
         public static int dead;
         public static int alive;
+        public static int error;
         public static int cheked;
+        public static int amount;
+
+        public static string currentTmie = DateTime.Now.ToString("dd-MM-yyyy hh-mm-ss");
+        public static int threads;
 
         public static bool Check(string proxy)
         {
@@ -29,7 +36,6 @@ namespace Proxy_Checker
                 var sent = ping.Send(proxy);
                 if (sent.Status == IPStatus.Success)
                 {
-                    working.Add(proxy);
                     return true;
                 }
                 else
@@ -37,9 +43,24 @@ namespace Proxy_Checker
                     return false;
                 }
             }
-            finally
+            catch(Exception ex)
             {
-                
+                Console.WriteLine($"[#] {proxy}: has failed.", Console.ForegroundColor = ConsoleColor.Blue);
+                error += 1;
+                return false;
+            }
+        }
+
+        public static void writeFile(string code)
+        {
+            try
+            {
+                string file = Environment.CurrentDirectory + @"\\Results\\[Alive] " + currentTmie + ".txt";
+                File.AppendAllText(file, code + Environment.NewLine);
+            } catch(Exception ex)
+            {
+                Console.WriteLine($"[#] {code}: Failed to save", Console.ForegroundColor = ConsoleColor.Blue);
+                error += 1;
             }
         }
 
@@ -48,6 +69,7 @@ namespace Proxy_Checker
         {
             Console.Title = "Proxy Checker by wulu#0827";
             Console.ForegroundColor = ConsoleColor.Magenta;
+
 
             string wulu = @"
                                     ____    __    ____  __    __   __       __    __  
@@ -59,78 +81,83 @@ namespace Proxy_Checker
 ";
             Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", wulu));
 
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("Open a proxy file");
+            Console.WriteLine("[?] How many threads?", Console.ForegroundColor = ConsoleColor.Blue);
+            Console.Write("> ");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            threads = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("[!] Open a proxy file", Console.ForegroundColor = ConsoleColor.Blue);
+            Console.Write("> ");
 
             Thread.Sleep(500);
 
             OpenFileDialog ofd = new OpenFileDialog();
             ofd.Filter = "Text files (*.txt)|*.txt";
             ofd.Title = "Proxies";
-            RichTextBox rtb = new RichTextBox();
 
             if (ofd.ShowDialog() == DialogResult.OK)
             {
                 rtb.Text = File.ReadAllText(ofd.FileName);
             }
 
+            Console.Write($"Found {rtb.Lines.Count().ToString()} proxies.", Console.ForegroundColor = ConsoleColor.Magenta);
+            Thread.Sleep(1000);
             Console.Clear();
+
+            Directory.CreateDirectory(Environment.CurrentDirectory + @"\\Results\\");
 
             Console.ForegroundColor = ConsoleColor.Magenta;
             Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", wulu));
 
-            int amount = rtb.Lines.Count();
+            amount = rtb.Lines.Count();
 
-            List<string> wo = new List<string>();
+            Console.ForegroundColor = ConsoleColor.Blue;
 
-            Parallel.ForEach(rtb.Lines, s =>
+            Thread thread = new Thread(loop);
+            thread.Start();         
+        }
+
+        public static void loop()
+        {
+            ParallelLoopResult parallel = Parallel.ForEach(rtb.Lines, s =>
             {
+                new ParallelOptions
+                {
+                    MaxDegreeOfParallelism = threads
+                };
+
                 cheked += 1;
 
-                string noport = s.Split(':')[0];
+                string noport = s.ToString().Split(':')[0];
 
                 if (Check(noport))
                 {
                     Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine(s);
+                    Console.WriteLine($"[+] {s}");
                     alive += 1;
-                    wo.Add(s);
+                    writeFile(s);
                 }
                 else
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine(s);
+                    Console.WriteLine($"[-] {s}");
                     dead += 1;
                 }
                 remaining = (amount - cheked);
-                Console.Title = "Proxy Checker by wulu#0827 | Remaining: " + remaining.ToString() + "/" + amount + " | DEAD: " + dead.ToString() + " | ALIVE: " + alive;
+                Console.Title = "Proxy Checker by wulu#0827 | Remaining: " + remaining.ToString() + "/" + amount + " | DEAD: " + dead.ToString() + " | ALIVE: " + alive + " | ERRORS: "+ error.ToString();
 
             });
 
-            Console.ForegroundColor = ConsoleColor.Blue;
-
-            Console.WriteLine("Finished checking proxies. Save to a file");
-            Console.Beep();
-            Thread.Sleep(500);
-
-            SaveFileDialog sfd = new SaveFileDialog();
-            sfd.Filter = ofd.Filter = "Text files (*.txt)|*.txt";
-            sfd.Title = "Save alive proxies";
-            sfd.FileName = alive.ToString();
-
-            if (sfd.ShowDialog() == DialogResult.OK)
+            if (parallel.IsCompleted)
             {
-                rtb.Text = "";
+                Console.WriteLine("[!] Finished checking proxies. Saved.", Console.ForegroundColor = ConsoleColor.Blue);
 
-                foreach (string s in wo.ToArray())
-                {
-                    rtb.AppendText(s + "\n");
-                }
-                rtb.SaveFile(Path.GetFullPath(sfd.FileName), RichTextBoxStreamType.PlainText);
-                Console.WriteLine("Finished saving proxies.");
+                Console.Beep();
+                Thread.Sleep(500);
+
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadLine();
             }
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadLine();
         }
     }
 }
